ed1d028c55c98c08732519e4dd04f4c41e55fac9
naimkhalifa <naimkhalifa@users.noreply.github.com>
2023-09-22 14:12:27 +0000

    Fix styling

--- src/FilamentTranslationsManagerServiceProvider.php
+++ src/FilamentTranslationsManagerServiceProvider.php
@@ -10,11 +10,11 @@ use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
+use Naimkhalifa\FilamentTranslationsManager\Commands\FilamentTranslationsManagerCommand;
+use Naimkhalifa\FilamentTranslationsManager\Testing\TestsFilamentTranslationsManager;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Naimkhalifa\FilamentTranslationsManager\Commands\FilamentTranslationsManagerCommand;
-use Naimkhalifa\FilamentTranslationsManager\Testing\TestsFilamentTranslationsManager;
 
 class FilamentTranslationsManagerServiceProvider extends PackageServiceProvider
 {
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,9 +16,9 @@ use Filament\Tables\TablesServiceProvider;
 use Filament\Widgets\WidgetsServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
+use Naimkhalifa\FilamentTranslationsManager\FilamentTranslationsManagerServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use Naimkhalifa\FilamentTranslationsManager\FilamentTranslationsManagerServiceProvider;
 
 class TestCase extends Orchestra
 {
